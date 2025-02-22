aebce7a16413dbd29b2235502fe52e6f7a4a45c1
gklav <gclaverie0@gmail.com>
2025-02-22 11:56:56 +0100

    feat: add social links

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer()
 {
     return (
-        <footer className="relative z-1 w-full flex pb-3 text-xs justify-center items-center text-align-center">
+        <footer className="relative z-1 w-full flex pt-10 pb-3 text-xs justify-center items-center text-align-center">
             <p>© {new Date().getFullYear()} Kaal Berry</p>
         </footer>
     );
